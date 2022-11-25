7f1557c897d9e9fcf29a541bde814534463c2312
Helen Lazarotti <74218577+HelenLazarotti@users.noreply.github.com>
2022-11-25 14:49:11 -0300

    Create Footer.jsx

---
+++ src/components/layout/Footer.jsx
@@ -0,0 +1,11 @@
+import styles from './Footer.module.css'
+
+function Footer(){
+    return (
+        <footer className={styles.footer}>
+            <p>Criado por HelenDev - 2022</p>
+        </footer>
+    )
+}
+
+export default Footer
